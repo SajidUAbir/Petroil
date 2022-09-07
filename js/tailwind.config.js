8c5d610392df84a9a6ad077a5394c5b9a1b98bbe
tushari789 <95566837+tushari789@users.noreply.github.com>
2022-09-08 03:15:32 +0600

    nav and menu added

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -1,17 +1,20 @@
 tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-            clifford: '#da373d',
-            'black': '#1F1F1F',
-            'lightBlack': '#282828',
-            'yellow': '#FFB800',
-            'red': '#F40404',
-            'alphaOfBlack': '#0009',
-          },
-          fontFamily: {
-            'Poppins': 'Poppins',
-          }
-        }
-      }
-    }
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        black: "#1F1F1F",
+        lightBlack: "#282828",
+        yellow: "#FFB800",
+        red: "#F40404",
+        alphaOfBlack: "#0009",
+      },
+      maxWidth: {
+        container: "1140px",
+      },
+      fontFamily: {
+        Poppins: "Poppins",
+      },
+    },
+  },
+};
