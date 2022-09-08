387a416fd1ced85c8a15e2b02675ba14295e1eb4
Imran Ahammed <imranahamed733@gmail.com>
2022-09-08 15:22:27 +0600

    brands and blog

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -1,23 +1,20 @@
 tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-            clifford: '#da373d',
-            'black': '#1F1F1F',
-            'lightBlack': '#282828',
-            'yellow': '#FFB800',
-            'red': '#F40404',
-            'alphaOfBlack': '#0009',
-          },
-          fontFamily: {
-            'Poppins': 'Poppins',
-          },
-          backgroundImage: {
-            banner: "url('images/banner.webp')",
-          },
-          maxWidth:{
-            container: '1140px',
-          },
-        }
-      }
-    }
+	theme: {
+		extend: {
+			colors: {
+				clifford: "#da373d",
+				black: "#1F1F1F",
+				lightBlack: "#282828",
+				yellow: "#FFB800",
+				red: "#F40404",
+				alphaOfBlack: "#0009",
+			},
+			fontFamily: {
+				poppins: "Poppins",
+			},
+			maxWidth: {
+				container: "1140px",
+			},
+		},
+	},
+};
